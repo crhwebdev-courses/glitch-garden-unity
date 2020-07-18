1ae34a74c4f5a9c3129aa7b24444a9c1693b4099
Carl Hain <crhain@gmail.com>
2020-07-18 11:06:17 -0500

    Create 'Level Loader' to load between scenes--currently, it loads Start Screen from Splash Screen after a short delay

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int _splashDelay = 4;
+
+    private int _splashScreen = 0;
+    private int _startScreen = 1;
+    private int _currentSceneIndex;
+    
+    private void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(_currentSceneIndex == _splashScreen)
+        {
+            StartCoroutine(LoadScreenAfterDelay(_startScreen, _splashDelay));
+        }
+        
+    }
+    
+    private IEnumerator LoadScreenAfterDelay(int screen, int delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(screen);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+}
