5c9335ed34d99afdce33e26ed5990b703f6f958e
Carl Hain <crhain@gmail.com>
2020-07-18 17:00:26 -0500

    Lizard attacker now moves across the screen from right to left

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+
+    [Range(0f, 5f)] [SerializeField] private float _walkSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.left * Time.deltaTime * _walkSpeed);
     }
 }
