6f807404295e6aecd2d2415391f62f822e2cfeff
Carl Hain <crhain@gmail.com>
2020-07-19 10:45:49 -0500

    Spawn explosion particle effect on lizard death

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,17 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private float _health = 100f;
+    [SerializeField] private GameObject _deathVFX;
 
     public void TakeDamage(float damage)
     {
         _health -= damage;
         if (_health <= 0)
         {
+            
+            TriggerDeathVFX();
             Destroy(gameObject);
+
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!_deathVFX){return;}
+
+        GameObject deathVFXObject = Instantiate(_deathVFX, transform.position, Quaternion.identity);
+        Destroy(deathVFXObject, 1f);
     }
 }
