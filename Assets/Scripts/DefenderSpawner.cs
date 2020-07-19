74cad951939aa3baff12a1f16e1334a01d7d3e2a
Carl Hain <crhain@gmail.com>
2020-07-19 11:21:51 -0500

    Spawn defenders in game area when mouse is clicked

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+    private void SpawnDefender(Vector2 spawnPos)
+    {
+        GameObject newDefender = Instantiate(
+            _defender, 
+            spawnPos, 
+            Quaternion.identity) as GameObject;
+    }
+}
