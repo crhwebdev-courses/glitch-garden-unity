3a7bce2fdc2746c08ba90b497be3a6328d34f9d2
Carl Hain <crhain@gmail.com>
2020-07-20 15:43:44 -0500

    Add script to trophy to increase star points and animate with a floating star icon

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int _starCost = 100;
+
+    public void AddStars(int amount)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
 }
