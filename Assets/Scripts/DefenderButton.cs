eb5fd4ece4578872c221fec13253bbfdfa469fa2
Carl Hain <crhain@gmail.com>
2020-07-19 22:53:30 -0500

    Add selector panel at buttom of screen with buttons for cactus and trophy defenders

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    private Color _defaultColor;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();        
+    }
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(82, 81, 81, 255);
+        }
+
+        _spriteRenderer.color = Color.white;
+    }   
+}
