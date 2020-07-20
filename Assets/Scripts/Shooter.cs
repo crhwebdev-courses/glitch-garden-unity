7903fd71b32530a7f13f9d5425b11b14b7cae5fb
Carl Hain <crhain@gmail.com>
2020-07-20 17:31:05 -0500

    Set up an animation parameter so we can transition between idle and attacking animations on our cacti from script--now cacti will only attack when an attacker is in the lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,7 +4,59 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] private GameObject projectile, gun;    
+    [SerializeField] private GameObject projectile, gun;
+    private AttackerSpawner _myLaneSpawner;
+    private AttackerSpawner[] _spawners;
+    private Animator _animator;
+
+    private void Start()
+    {        
+        SetLaneSpawner();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            _animator.SetBool("isAttacking", true);
+        }
+        else
+        {   
+            _animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        _spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in _spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+            {
+                _myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        // if my lane spawner child count less than or equal to 0
+        // return false
+        if(_myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+        
+    }
 
     public void Fire()
     {
