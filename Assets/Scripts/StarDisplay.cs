293defc02e95fa2927d4a2225cb97d1e57ca0adf
Carl Hain <crhain@gmail.com>
2020-07-20 14:55:21 -0500

    Fix Lizard to use a parent and child relationship--needed to redo animations as well

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] private int _stars = 100;
+    private Text _starText;
+    
+    void Start()
+    {
+        _starText = GetComponent<Text>();
+        UpdateDisplay();
+    }       
+
+    private void UpdateDisplay()
+    {
+        _starText.text = _stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        _stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        
+        if(_stars >= amount) 
+        {
+            _stars -= amount;
+            UpdateDisplay();
+        }        
+    }
+}
