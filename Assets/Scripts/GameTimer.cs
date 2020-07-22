d1c5860bc2059574ab1bce49fc052637b3341633
Carl Hain <crhain@gmail.com>
2020-07-22 15:39:52 -0500

    Add game timer with animated fox and progress it based on game time that has passed

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer in seconds")]
+    [SerializeField] private float _levelTime = 10;
+    
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / _levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= _levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("level timer expired!");
+        }
+    }
+}
