8622728506f207785e831b75ac5330699bcef238
Carl Hain <crhain@gmail.com>
2020-07-22 17:36:56 -0500

    Hook up buttons on menu screen and game over screen

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -43,6 +43,11 @@ public class LevelLoader : MonoBehaviour
         SceneManager.LoadScene(_startScreen);
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
     private IEnumerator LoadScreenAfterDelay(int screen, int delay)
     {
         yield return new WaitForSeconds(delay);
